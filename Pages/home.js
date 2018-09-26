f3723dc2d01e76eaea00d178b303eadc7d31a11d
Lukas Hermansson <lukas@lukashermansson.me>
2018-09-26 11:32:06 +0200

    Fixed bug where page change would cause linne not to play

--- Pages/home.js
+++ Pages/home.js
@@ -1,18 +1,19 @@
 
-let audio = new Audio('https://upload.wikimedia.org/wikipedia/commons/6/68/Sv-Carl_von_Linn%C3%A9.ogg');
 
 $(document).ready(function () {
-    $("#vonlinne").click(klickadKnapp);
-    console.log("laddad");
+    let audio = new Audio('https://upload.wikimedia.org/wikipedia/commons/6/68/Sv-Carl_von_Linn%C3%A9.ogg');
+    $("#vonlinne").unbind( "click" );
+    $("#vonlinne").click({ljud: audio}, klickadKnapp);
+    
 
     audio.addEventListener("ended", function(){
         $("#linnebild").attr("src", "");
    });
 });
 
-function klickadKnapp(){
+function klickadKnapp(event){
     console.log("klickad");
-    audio.play();
+    event.data.ljud.play();
     $("#linnebild").attr("src", "linne.jpg");
 }
 
